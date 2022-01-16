b310bb6cc8bd3cc52be4881ddfcd760e66e8f18d
Logan Davis <Ldavis96@live.com>
2022-01-16 13:09:13 -0500

    final styling

--- src/page.template.js
+++ src/page.template.js
@@ -13,43 +13,40 @@ module.exports = (team) => {
   <body>
       <div class="container-fluid">
           <div class="row">
-              <div class="col-12 jumbotron mb-3 team-heading">
+              <div class="col-12 jumbotron bg-danger text-white mb-3 team-heading">
                   <h1 class="text-center">My Team</h1>
               </div>
           </div>
       </div>
       <div class="container">
-          <div class="row">
+      <div class="flex-row">
               <div class="team-area col-12 d-flex justify-content-center">
                   ${team.map(teamMember => {
         if (teamMember.officeNumber != null) {
             return `
-                          <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                            <h5>${teamMember.name}</h5>
-                                <h5 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h5>
-                                <p class="card-text">(${teamMember.id}, ${teamMember.officeNumber}, <a href="mailto:${teamMember.email}">${teamMember.email}</a>)</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                            </div>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body text-center">
+                        <h3 class="bg-primary text-white">${teamMember.name}</h3>
+                        <h5 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h5>
+                        <p class="card-text"><h5>ID:</h5>${teamMember.id} <h5>Office Number:</h5>${teamMember.officeNumber} <h5>Email:</h5><a href="mailto:${teamMember.email}">${teamMember.email}</a></p>
+                        </div>
+                    </div>
                           `
         } if (teamMember.github != null) {
             return `<div class="card" style="width: 18rem;">
-                          <div class="card-body">
-                          <h5>${teamMember.name}</h5>
+                          <div class="card-body text-center">
+                          <h3 class="bg-primary text-white">${teamMember.name}</h3>
                             <h5 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h5>
-                            <p class="card-text">(${teamMember.id}, <a href="https://github.com/${teamMember.github}" target="_blank" rel="noopener noreferrer">${teamMember.github}</a>,<a href="mailto:${teamMember.email}">${teamMember.email}</a>)</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <p class="card-text"><h5>ID:</h5>${teamMember.id} <h5>Github:</h5><a href="https://github.com/${teamMember.github}" target="_blank" rel="noopener noreferrer">${teamMember.github}</a> <h5>Email:</h5><a href="mailto:${teamMember.email}">${teamMember.email}</a></p>
                           </div>
                         </div>
                           `
         } if (teamMember.school != null) {
             return `<div class="card" style="width: 18rem;">
-                          <div class="card-body">
-                          <h5>${teamMember.name}</h5>
+                          <div class="card-body text-center">
+                          <h3 class="bg-primary text-white">${teamMember.name}</h3>
                             <h5 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h5>
-                            <p class="card-text">(${teamMember.id}, ${teamMember.school}, <a href="mailto:${teamMember.email}">${teamMember.email}</a>)</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <p class="card-text"><h5>ID:</h5>${teamMember.id} <h5>University:</h5>${teamMember.school} <h5>Email:</h5><a href="mailto:${teamMember.email}">${teamMember.email}</a></p>
                           </div>
                         </div>
                           `
