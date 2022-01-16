2cb7c3f4d4e3a724834f0e8ee24e9f2b56e855e9
Logan Davis <Ldavis96@live.com>
2022-01-16 12:33:43 -0500

    finished object storage and page template HTML

--- index.js
+++ index.js
@@ -8,111 +8,124 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const newManagerPrompt = () => {
-inquirer
-    .prompt([
-    {
-        type: "input",
-        name: "managerName",
-        message: "Please enter your name",
-        },
-        {
-        type: "input",
-        name: "managerId",
-        message: "Please enter your ID number",
-        },
-        {
-        type: "input",
-        name: "managerEmail",
-        message: "Please enter your email",
-        },
-        {
-        type: "input",
-        name: "managerOfficeNumber",
-        message: "Please enter your office number",
-        },
-    ]).then((answers) => {
-        newTeamMemberPrompt();
+const Employees = [];
+
+
+const teamBuilder = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Please enter your name",
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Please enter your ID number",
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please enter your email",
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "Please enter your office number",
+            },
+        ]).then((answers) => {
+            Employees.push(new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber));
+            newTeamMemberPrompt();
         });
-    };
+};
 
 newTeamMemberPrompt = () => {
-inquirer
-    .prompt([
-    {
-        type: "list",
-        name: "role",
-        message: "Who would you like to add?",
-        choices: ["Engineer", "Intern"],
-    }]).then(userChoice => {
-        switch (userChoice.role) {
-            case"Engineer":
-            addEngineer();
-            break;
-            case"Intern":
-            addIntern();
-            break;
-            default:
-        };
-    })
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "Who would you like to add?",
+                choices: ["Engineer", "Intern", "I am finished building my team."],
+            }]).then(userChoice => {
+                switch (userChoice.role) {
+                    case "Engineer":
+                        addEngineer().then(answers => {
+                            Employees.push(new Engineer (answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub));
+                            newTeamMemberPrompt();
+                        })
+                        break;
+                    case "Intern":
+                        addIntern().then(answers => {
+                            Employees.push(new Intern (answers.internName, answers.internId, answers.internEmail, answers.internSchool));
+                            newTeamMemberPrompt();
+                        })
+                        break;
+                    default:
+                        fs.writeFileSync("./dist/Team.html", generatePage(Employees));
+                };
+            })
 };
 
 
-    function addEngineer () {
-        inquirer
-            .prompt([
-                {
+function addEngineer() {
+    return inquirer
+        .prompt([
+            {
                 type: "input",
                 name: "engineerName",
                 message: "Please enter your name",
-                },
-                {
+            },
+            {
                 type: "input",
                 name: "engineerId",
                 message: "Please enter your ID number",
-                },
-                {
+            },
+            {
                 type: "input",
                 name: "engineerEmail",
                 message: "Please enter your email",
-                },
-                {
+            },
+            {
                 type: "input",
                 name: "engineerGithub",
                 message: "Please enter github username",
-                }
-            ])}
+            }
+        ])
+}
 
-    function addIntern () {
-        inquirer
-            .prompt([
-                {
+function addIntern() {
+    return inquirer
+        .prompt([
+            {
                 type: "input",
                 name: "internName",
                 message: "Please enter your name",
-                },
-                {
+            },
+            {
                 type: "input",
                 name: "internId",
                 message: "Please enter your ID number",
-                },
-                {
+            },
+            {
                 type: "input",
                 name: "internEmail",
                 message: "Please enter your email",
-                },
-                {
+            },
+            {
                 type: "input",
                 name: "internSchool",
                 message: "Please enter your school",
-                }
-            ])}
+            }
+        ])
+}
 
 
-          function init() {
-            console.log("Please build your team!");
-            newManagerPrompt();
-          }
-          
-        //   Function call to initialize app
-          init();
+function init() {
+    console.log("Please build your team!");
+    teamBuilder();
+}
+
+//   Function call to initialize app
+init();
--- src/page.template.js
+++ src/page.template.js
@@ -1,1 +1,64 @@
-
+module.exports = (team) => {
+    return `
+      <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-3 team-heading">
+                  <h1 class="text-center">My Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="team-area col-12 d-flex justify-content-center">
+                  ${team.map(teamMember => {
+        if (teamMember.officeNumber != null) {
+            return `
+                          <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                            <h5>${teamMember.name}</h5>
+                                <h5 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h5>
+                                <p class="card-text">(${teamMember.id}, ${teamMember.officeNumber}, <a href="mailto:${teamMember.email}">${teamMember.email}</a>)</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                            </div>
+                          `
+        } if (teamMember.github != null) {
+            return `<div class="card" style="width: 18rem;">
+                          <div class="card-body">
+                          <h5>${teamMember.name}</h5>
+                            <h5 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h5>
+                            <p class="card-text">(${teamMember.id}, <a href="https://github.com/${teamMember.github}" target="_blank" rel="noopener noreferrer">${teamMember.github}</a>,<a href="mailto:${teamMember.email}">${teamMember.email}</a>)</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+                          `
+        } if (teamMember.school != null) {
+            return `<div class="card" style="width: 18rem;">
+                          <div class="card-body">
+                          <h5>${teamMember.name}</h5>
+                            <h5 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h5>
+                            <p class="card-text">(${teamMember.id}, ${teamMember.school}, <a href="mailto:${teamMember.email}">${teamMember.email}</a>)</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+                          `
+        }
+    })}
+              </div>
+          </div>
+      </div>
+  </body>
+  </html>
+      `;
+};
